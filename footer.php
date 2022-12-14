1496c76078c2f1109c1829249a6d3d92ffe3f824
thangnm_dolphin <thangnm@dolphin-technologies.net>
2022-12-15 00:39:37 +0700

    finish footer layout

--- footer.php
+++ footer.php
@@ -2,11 +2,133 @@
     <div class="flex flex-col">
         <div class="pt-24 px-24 flex-col flex">
             <div class="font-medium text-base text-[#183401] uppercase">Newsletter</div>
-            <div class="ml-56 font-prata laptop:text-7xl text-[84px] !leading-[6rem] text-[#183401]">Theo dõi <span class="text-[#53802B]">Nguyên Ninh<span></div>
-
+            <div class="mt-8 ml-56 font-prata laptop:text-7xl text-[84px] !leading-[6rem] text-[#183401]">Theo dõi <span
+                    class="text-[#53802B]">Nguyên Ninh<span></div>
+            <div class="ml-56 flex gap-16 mt-16 mb-24">
+                <div class="font-medium  w-1/2 text-base leading-[26px] text-[#52544F]">Điền email vào đây để không bỏ
+                    lỡ những câu chuyện ấm ám, những tin tức, hay khuyến mãi của Nguyên Ninh gửi tới hộp thư của bạn.
+                </div>
+                <div class="flex flex-col w-1/2 gap-12">
+                    <div class="flex justify-between border-b-[0.5px] border-[#9EA09C]">
+                        <input type="text" name="" id="" placeholder="Họ & tên"
+                            class="py-4 bg-[#F9F8F1] focus:outline-none flex-grow" />
+                        <i class="fa-light fa-user self-center"></i>
+                    </div>
+                    <div class="flex justify-between border-b-[0.5px] border-[#9EA09C]">
+                        <input type="text" name="" id="" placeholder="Địa chỉ email"
+                            class="py-4 bg-[#F9F8F1] focus:outline-none flex-grow" />
+                        <i class="fa-light fa-envelope self-center"></i>
+                    </div>
+                    <button
+                        class="px-6 py-4 w-1/3 rounded-[26px] bg-[#D6E1CC] text-[#306701]
+                     focus:border-[#98B380] focus:border-[4.8px] disabled:bg-[#D1D2D0] disabled:text-white uppercase">Đăng
+                        ký</button>
+                </div>
+            </div>
         </div>
     </div>
-    <?php wp_footer(); ?>
-    </body>
+</div>
+<div class="w-full flex bg-[#183401]">
+    <div class="flex-col gap-12 pl-14 py-24 w-1/3 ">
+        <div class="font-prata font-normal text-[2.5rem] laptop:text-[2rem] leading-[3rem] text-white w-2/3">Bánh cốm
+            Nguyên Ninh</div>
+        <div class="flex-col gap-16 mt-12">
+            <div class="flex justify-between pb-[12px] mt-6 border-b-[0.5px] border-[#285601]">
+                <div class="font-medium text-sm leading-[22px] text-white">Địa chỉ</div>
+                <div class="font-bold text-xs leading-[22px] text-white">Số 11 Hàng Than, Ba Đình, Hà Nội</div>
+            </div>
+            <div class="flex justify-between pb-[12px] mt-6 border-b-[0.5px] border-[#285601]">
+                <div class="font-medium text-sm leading-[22px] text-white">Hotline</div>
+                <div class="font-bold text-xs leading-[22px] text-white">(0243) 828 3573 - 090 220 8459</div>
+            </div>
+            <div class="flex justify-between pb-[12px] mt-6 border-b-[0.5px] border-[#285601]">
+                <div class="font-medium text-sm leading-[22px] text-white">Email</div>
+                <div class="font-bold text-xs leading-[22px] text-white">banhcomnguyenninh@gmail.com</div>
+            </div>
+        </div>
+        <div class="flex mt-24 justify-end">
+            <a href="#" class="text-white text-base block">FACEBOOK
+                <i class="fa-light fa-arrow-up rotate-45 text-white"></i>
+            </a>
+            <div class="transform -rotate-90 w-4 h-0.5 border-white border mx-6 border-solid mt-[8px]"></div>
+            <a href="#" class="text-white text-base block">INSTAGRAM
+                <i class="fa-light fa-arrow-up rotate-45 text-white"></i>
+            </a>
+        </div>
+    </div>
+    <div class="flex-col gap-16 py-24 ml-24 w-2/3 pr-20">
+        <div class="flex">
+            <div class="flex-col gap-8 ml-12">
+                <div class="text-white font-medium text-base leading-4 uppercase">sản phẩm</div>
+                <div class="flex-col">
+                    <div class="mt-8">
+                        <a href="#" class="text-white text-xs block">Bánh cốm Nguyên Ninh
+                            <i class="fa-light fa-arrow-up rotate-45 text-white"></i>
+                        </a>
+                    </div>
+                    <div class="mt-8">
+                        <a href="#" class="text-white text-xs block">Chè Thái Nguyên
+                            <i class="fa-light fa-arrow-up rotate-45 text-white"></i>
+                        </a>
+                    </div>
+                    <div class="mt-8">
+                        <a href="#" class="text-white text-xs block">Bánh xu xê
+                            <i class="fa-light fa-arrow-up rotate-45 text-white"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-col gap-8 ml-12">
+                <div class="text-white font-medium text-base leading-4 uppercase">về nguyên ninh</div>
+                <div class="flex-col">
+                    <div class="mt-8">
+                        <a href="#" class="text-white text-xs block">Giá trị cốt lõi
+                            <i class="fa-light fa-arrow-up rotate-45 text-white"></i>
+                        </a>
+                    </div>
+                    <div class="mt-8">
+                        <a href="#" class="text-white text-xs block">Niềm tự hào
+                            <i class="fa-light fa-arrow-up rotate-45 text-white"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-col gap-8 ml-12">
+                <div class="text-white font-medium text-base leading-4 uppercase">câu chuyện</div>
+                <div class="flex-col">
+                </div>
+            </div>
+            <div class="flex-col gap-8 ml-12">
+                <div class="text-white font-medium text-base leading-4 uppercase">liên hệ</div>
+                <div class="flex-col">
 
-    </html>
+                </div>
+            </div>
+        </div>
+        <div class="flex mt-36 justify-end pb-8 border-b border-[#285601]">
+            <p class="uppercase font-medium text-sm text-white mr-6">NGÔN NGỮ:</p>
+            <a href="#" class="text-white text-xs block">VI
+            </a>
+            <div class="transform -rotate-90 w-4 h-0.5 border-white border mx-6 border-solid mt-[8px]"></div>
+            <a href="#" class="text-white text-xs block">ENG
+            </a>
+        </div>
+        <div class="flex justify-between">
+            <div class="flex pt-8 mt-[3px]">
+                <a href="#" class="text-white text-base mr-12">COOKIE
+                </a>
+                <a href="#" class="text-white text-base ">QUYỀN RIÊNG TƯ
+                </a>
+            </div>
+            <div class="pt-8 mt-[3px]">
+                <p class="uppercase font-medium text-sm text-white mr-6"><i class="fa-regular fa-copyright text-white  mr-[3px]"></i> NGUYÊN NINH 2022</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+<?php wp_footer(); ?>
+</body>
+
+</html>
